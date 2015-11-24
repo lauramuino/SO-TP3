5a0304ec05f6e3b95a0c971ff1cb83467046d991
quintanj <jorge.quintana.81@gmail.com>
2015-11-24 01:34:05 -0300

    - Modificados los problemas de alineacion del codigo

--- codigo/srv.c
+++ codigo/srv.c
@@ -2,7 +2,7 @@
 
 int comparacion(int rank_i, int rank_j){
 // Si el primero es menor que el segundo -> TRUE
-	assert (rank_i != rank_j);
+    assert (rank_i != rank_j);
     if(rank_i > rank_j){
         return FALSE;
     }else{
@@ -20,18 +20,18 @@ void servidor(int mi_cliente)
     int mi_rank = mi_cliente -1; //el cliente tiene identificador -1 a su servidor
     int server; // iterador para los fors
     int pendientes[cant_ranks/2];
-	int a_servers_muertos[cant_ranks/2];
+    int a_servers_muertos[cant_ranks/2];
     int acumulador = 0;
     int reloj = 0;
     int our_number = 0;
     int buffer;
     int servers_muertos = 0;
-	char debug_msg[255];
+    char debug_msg[255];
 
     int iterador; // Inicializando pendientes para RESPUESTAS DEFERIDAS
     for(iterador = 0; iterador < cant_ranks/2; iterador++){
         pendientes[iterador] = 0;
-		a_servers_muertos[iterador] = 0; // Inicializando arreglo de servers muertos
+        a_servers_muertos[iterador] = 0; // Inicializando arreglo de servers muertos
     }
 
     
@@ -46,7 +46,7 @@ void servidor(int mi_cliente)
                 //pedido de mi cliente
                 reloj++;
                 our_number = reloj;
-				sprintf(debug_msg, "Mi cliente - rank %i - solicita acceso exclusivo", mi_cliente);
+                sprintf(debug_msg, "Mi cliente - rank %i - solicita acceso exclusivo", mi_cliente);
                 debug(debug_msg);
                 assert(hay_pedido_local == FALSE);
                 
@@ -54,21 +54,21 @@ void servidor(int mi_cliente)
 
                 debug("Obteniendo permiso de los demás servers");
                 if( (cant_ranks == 2) || (servers_muertos == (cant_ranks/2) -1 ) ){ // No hay otros servers o se murieron todos
-					sprintf(debug_msg, "No existen otros servers o murieron todos");
-					debug(debug_msg);
+                    sprintf(debug_msg, "No existen otros servers o murieron todos");
+                    debug(debug_msg);
                     MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
                     cliente_en_zona_critica = TRUE;
                 }else{
                     for (server = 0; server < cant_ranks/2; server++){
                         //veo no ser yo y no le envio a los muertos
                         if(((server * 2) != mi_rank) && (a_servers_muertos[server] == 0)){
-							sprintf(debug_msg, "Pidiendo permiso al server rank %i con seq: %i", server*2, our_number);
-			    			debug(debug_msg);
+                            sprintf(debug_msg, "Pidiendo permiso al server rank %i con seq: %i", server*2, our_number);
+                            debug(debug_msg);
                             //MPI_Ssend(&buffer, 1, MPI_INT, 2*server, TAG_PEDIDO, COMM_WORLD); // Provoca DEADLOCK - HOLD & WAIT
                             MPI_Send(&buffer, 1, MPI_INT, 2*server, TAG_PEDIDO, COMM_WORLD);
                         }
                     }
-					debug("Marcando que hay pedido local = TRUE");
+                    debug("Marcando que hay pedido local = TRUE");
                     hay_pedido_local = TRUE;
                 }
 
@@ -76,46 +76,46 @@ void servidor(int mi_cliente)
                 //me pide permiso un server
                 //int otro_reloj = buffer;
                 if( buffer > reloj){ 
-					sprintf(debug_msg, "Se actualizo el reloj del server %i - valor antiguo: %i - nuevo valor: %i", mi_rank, reloj, buffer);
-					debug(debug_msg);
-					reloj = buffer; //actualizo mi reloj de ser necesario
-				}
-				// NO ME CONVENCE - QUE PASA SI YO YA ENVIE PEDIDO DE MI CLIENTE?
-				// CREO QUE NO PASA NADA,YA QUE YA ENVIÉ Y NO ENVIO DE NUEVO
+                    sprintf(debug_msg, "Se actualizo el reloj del server %i - valor antiguo: %i - nuevo valor: %i", mi_rank, reloj, buffer);
+                    debug(debug_msg);
+                    reloj = buffer; //actualizo mi reloj de ser necesario
+                }
+                // NO ME CONVENCE - QUE PASA SI YO YA ENVIE PEDIDO DE MI CLIENTE?
+                // CREO QUE NO PASA NADA,YA QUE YA ENVIÉ Y NO ENVIO DE NUEVO
 
                 if(cliente_en_zona_critica){
-					sprintf(debug_msg, "Tengo cliente en zona crítica - dejando pedido de server rank %i pendiente", origen);
-					debug(debug_msg);
+                    sprintf(debug_msg, "Tengo cliente en zona crítica - dejando pedido de server rank %i pendiente", origen);
+                    debug(debug_msg);
                     pendientes[origen/2] = 1;
                 } else {
                     if(!hay_pedido_local){
                         //debug("Dándole permiso a algun server");
-						sprintf(debug_msg, "No hay pedido local - Dando permiso al server rank %i con seq: %i", origen , buffer);
-						debug(debug_msg);
+                        sprintf(debug_msg, "No hay pedido local - Dando permiso al server rank %i con seq: %i", origen , buffer);
+                        debug(debug_msg);
                         MPI_Send(NULL, 0, MPI_INT, origen, TAG_OTORGADO, COMM_WORLD);
                     }else{
                         //hago las comparaciones de mierda
-						sprintf(debug_msg, "Hay pedido local - Viendo a quien le toca");
-						debug(debug_msg);
+                        sprintf(debug_msg, "Hay pedido local - Viendo a quien le toca");
+                        debug(debug_msg);
                         if(buffer == our_number){
                             if(comparacion(mi_rank, origen)){
-								// mi_rank < origen:
-								sprintf(debug_msg, "Hay pedido local - DIFIRIENDO server rank %i con seq: %i - MISMO seq que yo", origen, buffer); 
-								debug(debug_msg);
+                                // mi_rank < origen:
+                                sprintf(debug_msg, "Hay pedido local - DIFIRIENDO server rank %i con seq: %i - MISMO seq que yo", origen, buffer); 
+                                debug(debug_msg);
                                 pendientes[origen/2] = 1;
                             }else{
-								sprintf(debug_msg, "Hay pedido local - GRANTING server rank %i con seq: %i - Menor RANK que yo", origen, buffer);
-								debug(debug_msg);
+                                sprintf(debug_msg, "Hay pedido local - GRANTING server rank %i con seq: %i - Menor RANK que yo", origen, buffer);
+                                debug(debug_msg);
                                 MPI_Send(NULL, 0, MPI_INT, origen,TAG_OTORGADO, COMM_WORLD);
                             }
                         }else{
                             if(buffer < our_number){
-								sprintf(debug_msg, "Hay pedido local - GRANTING server rank %i con seq: %i - Menor seq que yo", origen, buffer);
-								debug(debug_msg);
+                                sprintf(debug_msg, "Hay pedido local - GRANTING server rank %i con seq: %i - Menor seq que yo", origen, buffer);
+                                debug(debug_msg);
                                 MPI_Send(NULL, 0, MPI_INT, origen,TAG_OTORGADO, COMM_WORLD);
                             }else{
-								sprintf(debug_msg, "Hay pedido local - DIFIRIENDO server rank %i con seq:  %i - Yo tengo menor seq", origen, buffer);
-								debug(debug_msg);
+                                sprintf(debug_msg, "Hay pedido local - DIFIRIENDO server rank %i con seq:  %i - Yo tengo menor seq", origen, buffer);
+                                debug(debug_msg);
                                 pendientes[origen/2] = 1;
                             }
                         }
@@ -134,8 +134,8 @@ void servidor(int mi_cliente)
             for (iterador = 0; iterador < cant_ranks/2; iterador++){
                 if( (pendientes[iterador] == 1) && (iterador*2 != mi_rank) && (a_servers_muertos[iterador] == 0) ){
                     pendientes[iterador] = 0;
-					sprintf(debug_msg, "Otorgando GRANT a server rank %i", iterador *2);
-					debug(debug_msg);
+                    sprintf(debug_msg, "Otorgando GRANT a server rank %i", iterador *2);
+                    debug(debug_msg);
                     MPI_Send(NULL, 0, MPI_INT, iterador*2,TAG_OTORGADO, COMM_WORLD);
                 }
             }
@@ -151,8 +151,8 @@ void servidor(int mi_cliente)
                     //if( (pendientes[iterador] == 1) && (iterador*2 != mi_rank) ){
                     if( (pendientes[iterador] == 1) && (iterador*2 != mi_rank) && (a_servers_muertos[iterador] == 0) ){
                         //pendientes[iterador] = 0;
-						sprintf(debug_msg, "Otorgando GRANT a server rank %i", iterador *2);
-						debug(debug_msg);
+                        sprintf(debug_msg, "Otorgando GRANT a server rank %i", iterador *2);
+                        debug(debug_msg);
                         MPI_Send(NULL, 0, MPI_INT, iterador*2,TAG_OTORGADO, COMM_WORLD);
                     }
                 }
@@ -160,25 +160,25 @@ void servidor(int mi_cliente)
                 //y aviso que me voy
                 for (iterador = 0; iterador < cant_ranks/2; iterador++){
                     if( (iterador*2 != mi_rank) && (a_servers_muertos[iterador] == 0) ){
-						sprintf(debug_msg, "Avisando que TERMINE a server rank %i", iterador *2);
-						debug(debug_msg);
+                        sprintf(debug_msg, "Avisando que TERMINE a server rank %i", iterador *2);
+                        debug(debug_msg);
                         //MPI_Ssend(NULL, 0, MPI_INT, iterador*2, TAG_TERMINE, COMM_WORLD);
                         MPI_Send(NULL, 0, MPI_INT, iterador*2, TAG_TERMINE, COMM_WORLD);
-					}
+                    }
                 }
-				// y me voy - Vuelvo al MAIN
-				return;
+                // y me voy - Vuelvo al MAIN
+                return;
             }else{ // Me llegó de otro server
-				assert(origen % 2 == 0); // Es un server
+                assert(origen % 2 == 0); // Es un server
                 servers_muertos++;
-				a_servers_muertos[origen/2] = 1; // Lo marco como muerto
+                a_servers_muertos[origen/2] = 1; // Lo marco como muerto
                 pendientes[origen/2] = 0;
-				sprintf(debug_msg, "Se murió el server rank %i - Cantidad de servers muertos %i", origen, servers_muertos);
-				debug(debug_msg);
+                sprintf(debug_msg, "Se murió el server rank %i - Cantidad de servers muertos %i", origen, servers_muertos);
+                debug(debug_msg);
                 if(hay_pedido_local){
-					// Hace falta otorgarle agregar el GRANT del origen a esta altura?
-					// Creo que no - Debería haber otorgado el GRANT antes de enviar el TAG_TERMINE
-				}
+                    // Hace falta otorgarle agregar el GRANT del origen a esta altura?
+                    // Creo que no - Debería haber otorgado el GRANT antes de enviar el TAG_TERMINE
+                }
             }
         }
 
@@ -193,7 +193,7 @@ void servidor(int mi_cliente)
                 //el pedido fue cumplido, asi q.. ya no hay pedido local
                 MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
                 hay_pedido_local = FALSE;
-				acumulador = 0; // Reseteo acumulador
+                acumulador = 0; // Reseteo acumulador
             }
         }
         
